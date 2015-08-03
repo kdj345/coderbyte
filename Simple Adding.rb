fd9676b76a3f03bef525ff2596ecf7d0bf8501f2
David Dongjune Kim <01067334056@naver.com>
2015-08-03 11:49:30 +0900

    Create Simple Adding.rb

---
+++ Simple Adding.rb
@@ -0,0 +1,13 @@
+def SimpleAdding(num)
+
+  sum = 0
+  (1..num).each do |x|
+    sum += x
+  end
+  	sum
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)           
