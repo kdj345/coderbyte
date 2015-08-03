21b31fd68e2e5a2c5aa2c09a57c52b1d756198ed
David Dongjune Kim <01067334056@naver.com>
2015-08-03 11:50:56 +0900

    Create Time Convert.rb

---
+++ Time Convert.rb
@@ -0,0 +1,10 @@
+def TimeConvert(num)
+  return "#{num/60}:#{num%60}" 
+  # code goes here
+  return num 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)       
